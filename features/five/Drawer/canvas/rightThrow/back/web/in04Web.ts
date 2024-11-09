b62283d76a502ef95bed0acf6f14f62744c3813a
tomoki33kurita <tomoki33kurita@gmail.com>
2024-11-09 14:40:27 +0900

    fixed in04web

--- features/five/Drawer/canvas/rightThrow/back/web/in04Web.ts
+++ features/five/Drawer/canvas/rightThrow/back/web/in04Web.ts
@@ -1,14 +1,14 @@
 import { webKnots } from '@/util/canvas/lace/webKnots'
 import { webToCatchFaceConnect } from '../lace/webToCatchFaceConnect'
-import { middleSidePartOfWeb, topSidePartOfWeb } from './parts/sideParts'
+import { middleSidePartOfWeb2, topSidePartOfWeb2 } from './parts/sideParts'
 
 export const in04Web = (ctx: CanvasRenderingContext2D, webColor: string, laceColor: string, stitchColor: string): void => {
   ctx.lineWidth = 0.8
   ctx.strokeStyle = '#383838'
   // 横上パーツ
-  topSidePartOfWeb(ctx, webColor, laceColor, stitchColor)
+  topSidePartOfWeb2(ctx, webColor, laceColor, stitchColor)
   // 横下パーツ
-  middleSidePartOfWeb(ctx, webColor, laceColor, stitchColor, -10, 10, 1, 1)
+  middleSidePartOfWeb2(ctx, webColor, laceColor, stitchColor, -10, 10, 1, 1)
   // 縦パーツ
   ctx.fillStyle = webColor
   ctx.beginPath()
