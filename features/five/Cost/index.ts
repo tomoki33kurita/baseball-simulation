92085052f20c6964ab205e05b9c94a9341bb149f
tomoki33kurita <tomoki33kurita@gmail.com>
2024-11-10 15:02:18 +0900

    Added embroidery cost logic

--- features/five/Cost/index.ts
+++ features/five/Cost/index.ts
@@ -1,5 +1,6 @@
 import { Embroidery } from '@/types'
 import { FiveState } from '../types'
+import { getOrderType } from '../Components/Setters/logic'
 
 export const calculateFiveBaseCost = (state: FiveState): number => {
   const baseCost = state.orderType.price
@@ -12,11 +13,13 @@ export const calculateFiveOptionCost = (state: FiveState): number => {
 }
 
 export const calculateFiveEmbroideryCost = (state: FiveState): number => {
+  const { isCustomOrder } = getOrderType(state.orderType)
+  if (!isCustomOrder) return 0
   const embroideries = state.embroideries
   const embroideriesOfFiltered = embroideries.filter((e: Embroidery) => e.content.trim()?.length > 0)
-  const singlePrice = 2200
-  const shadowPrice = 2530
-  const edgePrice = 2750
+  const singlePrice = 1100
+  const shadowPrice = 1650
+  const edgePrice = 2200
   const costsOfEmbroideries = embroideriesOfFiltered
     .map((e: Embroidery) => {
       const isEdge = e.edgeColor.value !== 'none' //フチ付きかどうか
@@ -25,5 +28,8 @@ export const calculateFiveEmbroideryCost = (state: FiveState): number => {
       return cost // 2750
     }) // [2750, 2200, ...]
     .reduce((a: number, c: number) => a + c, 0) //
-  return costsOfEmbroideries
+  const existsDiscount = embroideriesOfFiltered.some((e) => e.edgeColor.value === 'none' && e.shadowColor.value === 'none')
+  const discount = existsDiscount ? 1100 : 0
+  const discounted = costsOfEmbroideries - discount
+  return discounted
 }
